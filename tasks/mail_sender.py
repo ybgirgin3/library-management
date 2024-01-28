234d295f8dec5117c4626c49d2e74de1e1c1ef7d
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2024-01-28 14:48:59 +0300

    weekly report is done

--- tasks/mail_sender.py
+++ tasks/mail_sender.py
@@ -23,12 +23,31 @@ class Mail:
     OVERDUE_SUBJECT = 'You Have Books with Overdue\'d Date'
     REPORT_SUBJECT = 'Your Weekly Report is Here'
 
-    def __init__(self, receiver_mails: Union[str, List[str]], overdue_data: Union[Dict, List[Dict]], mail_type: str = 'overdue'):
+    def __init__(self,
+                 receiver_mails: Union[str, List[str]],
+                 data: Union[Dict, List[Dict]],
+                 mail_type: str = 'overdue'
+        ) -> None:
         self.receiver_mails = [receiver_mails] if isinstance(receiver_mails, str) else receiver_mails
-        self.overdue_data = [overdue_data] if isinstance(overdue_data, str) else overdue_data
-        self.email = self.create_html_body(DataFrame(self.overdue_data), _type=mail_type)
+        self.data = [data] if isinstance(data, str) else data
+        self.email = self.create_html_body(DataFrame(self.data))
+        self.mail_type = mail_type
 
-    def send_overdue(self):
+    def overdue(self):
+        for rec in self.receiver_mails:
+            self.email['From'] = self.CREDENTIALS['EMAIL']
+            self.email['To'] = rec
+            self.email['Subject'] = self.OVERDUE_SUBJECT
+            self.send(rec)
+
+    def weekly_report(self):
+        for rec in self.receiver_mails:
+            self.email['From'] = self.CREDENTIALS['EMAIL']
+            self.email['To'] = rec
+            self.email['Subject'] = self.REPORT_SUBJECT
+            self.send(rec)
+
+    def send(self, receiver):
         # [{'id': 3, 'patron_id': 1, 'book_id': 2,
         #         'checkout_date': datetime.datetime(2024, 1, 27, 18, 46, 28, 897807),
         #         'refund_date': datetime.datetime(2024, 1, 27, 18, 46, 28, 897807), 'is_active': True}]
@@ -36,27 +55,16 @@ class Mail:
         self.smtp.starttls()
         self.smtp.login(self.CREDENTIALS['EMAIL'], self.CREDENTIALS['PASSWORD'])
 
-        # create dataframe from overdue_data dict
-        # dict_data = DataFrame(self.overdue_data)
-
-        # self.email = self.create_html_body(dict_data, _type='overdue')
-
-        for rec in self.receiver_mails:
-            self.email['From'] = self.CREDENTIALS['EMAIL']
-            self.email['To'] = rec
-            self.email['Subject'] = self.OVERDUE_SUBJECT
-
-            self.smtp.sendmail(self.CREDENTIALS['EMAIL'], rec, self.email.as_string())
-            time.sleep(2)
-
+        self.smtp.sendmail(self.CREDENTIALS['EMAIL'], receiver, self.email.as_string())
+        time.sleep(2)
         self.smtp.quit()
 
-    def create_html_body(self, data: DataFrame, _type: str):
+    def create_html_body(self, data: DataFrame):
         body = f"""
             <html>
               <body>
                 <p>Hi!<br>
-                {self.OVERDUE_SUBJECT if _type == 'overdue' else self.REPORT_SUBJECT}
+                {self.OVERDUE_SUBJECT if self.mail_type == 'overdue' else self.REPORT_SUBJECT}
                 {data.to_html(na_rep="", index=False).replace("<th>", "<th style = 'background-color: gray; color: white; text-align: center'>").replace("<td>", "<td style = 'text-align: start; padding: 10px;'>")}
                 <br>
                </p>
--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,9 +1,11 @@
 import datetime
+from typing import Dict
+from typing import List
 
 from celery import chain
 
 from models import PatronModel
-from routers.checkout import find_all
+from routers.checkout import find_all as checkout_findall
 from services.orm import ORM
 from tasks.beat import app
 from tasks.mail_sender import Mail
@@ -11,35 +13,60 @@ from tasks.mail_sender import Mail
 patron_orm = ORM(model='PatronModel')
 
 
-# get overdue tasks periodically
+# =========== checkouts begins ===============
 @app.task(name='task.get_overdues', bind=True)
 def get_overdues(self):
-    overdues = find_all(overdue=1).data
+    overdues = checkout_findall(overdue=1).data
     if len(overdues):
         overdues_as_dict = [o.to_dict() for o in overdues]
         return overdues_as_dict
         # app.send_task(name='task.reminder', args=[overdues_as_dict], queue='send_reminder_mail')
 
+@app.task(name='task.get_checkouts', bind=True)
+def get_checkouts(self):
+    checkouts_as_dict = [c.to_dict() for c in checkout_findall().data]
+    if not len(checkouts_as_dict) or checkouts_as_dict is None:
+        print('no checkouts found')
 
+    return checkouts_as_dict
+
+# =========== checkouts ends ===============
+
+
+# =========== mails starts ===============
 @app.task(name='task.reminder_mail', bind=True)
-def reminder(self, overdues: list[dict]):
-    [{'id': 3, 'patron_id': 1, 'book_id': 2,
-            'checkout_date': datetime.datetime(2024, 1, 27, 18, 46, 28, 897807),
-            'refund_date': datetime.datetime(2024, 1, 27, 18, 46, 28, 897807),
-            'is_active': True}]
+def reminder(self, overdues: List[Dict]):
+    # [{'id': 3, 'patron_id': 1, 'book_id': 2,
+    #         'checkout_date': datetime.datetime(2024, 1, 27, 18, 46, 28, 897807),
+    #         'refund_date': datetime.datetime(2024, 1, 27, 18, 46, 28, 897807),
+    #         'is_active': True}]
 
     # find receiver_mail from overdue
-    patron_ids: list = [d.get('patron_id') for d in overdues]
+    patron_ids: List = [d.get('patron_id') for d in overdues]
 
     # perform mail send
     for _id in patron_ids:
         rec: PatronModel = patron_orm.find_one({'id': _id})
-        m = Mail(receiver_mails=rec.email, overdue_data=overdues)
-        m.send_overdue()
+        m = Mail(receiver_mails=rec.email, data=overdues)
+        m.overdue()
+    return 'Overdue mail sending task is successfully finished'
 
 
+@app.task(name='task.reporter', bind=True)
+def reporter(self, checkouts: List[Dict]) -> str:
+    patron_ids: List = list(set([d.get('patron_id') for d in checkouts]))
 
-@app.task(name='task.reporter')
-def reporter():
+    # perform mail send
+    for _id in patron_ids:
+        rec: PatronModel = patron_orm.find_one({'id': _id})
+        m = Mail(receiver_mails=rec.email, data=checkouts, mail_type='report')
+        m.weekly_report()
+    return 'Weekly report sending task is successfully finished'
+
+# =========== mails ends ===============
+
+
+@app.task(name='task.process')
+def process():
     result_chain = chain(get_overdues.s(), reminder.s())
     print(result_chain())
